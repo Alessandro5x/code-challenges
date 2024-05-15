2d0111fd6b3cb7287141d38bee7e8c28a3201fc0
Alessandro <alessandro5@live.com>
2024-05-14 22:12:19 -0300

    feat: mergeSort in python

--- recruitment_Plataforms/quickSort.py
+++ recruitment_Plataforms/quickSort.py
@@ -11,3 +11,5 @@ def quicksort(arr):
 # Example usage
 arr = [3, 6, 8, 10, 1, 2, 1]
 print(quicksort(arr))  # Output: [1, 1, 2, 3, 6, 8, 10]
+
+## QuickSort is a highly efficient sorting algorithm and is based on the divide-and-conquer approach. It works by selecting a 'pivot' element from the array and partitioning the other elements into two sub-arrays, according to whether they are less than or greater than the pivot. The sub-arrays are then sorted recursively.
