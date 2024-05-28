2104e0ab8f21d41227fdd74a9e78a2f35ca1be8b
Alessandro <alessandro5@live.com>
2024-05-28 14:10:46 -0300

    feat: twoSum Problem implementation with hash Table

--- leet_Code/twoSum.py
+++ leet_Code/twoSum.py
@@ -1,7 +1,21 @@
 from typing import List
+# My first exercise using hash table 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)-1):
-                if (nums[i]+nums[j+1] == target):
-                    return [i,j+1]
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[nums[i]] = i
+
+        return []  # No solution found
+# Brute force
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(len(nums)-1):
+#                 if (nums[i]+nums[j+1] == target):
+#                     return [i,j+1]
