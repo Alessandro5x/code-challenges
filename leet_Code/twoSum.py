597d126997c8c443799dc9e1265776e4491e41a1
Alessandro <alessandro5@live.com>
2024-06-20 19:42:36 -0300

    feat: mergedtwoSortedLists problem

--- leet_Code/twoSum.py
+++ leet_Code/twoSum.py
@@ -1,17 +1,20 @@
 from typing import List
 # My first exercise using hash table 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(nums: List[int], target: int) -> List[int]:
         numMap = {}
         n = len(nums)
 
-        for i in range(n):
+        for i in range(1,n):
+            print(i)
             complement = target - nums[i]
             if complement in numMap:
                 return [numMap[complement], i]
             numMap[nums[i]] = i
 
         return []  # No solution found
+    
+    twoSum([1,2,3,4],5)
 # Brute force
 # class Solution:
 #     def twoSum(self, nums: List[int], target: int) -> List[int]:
